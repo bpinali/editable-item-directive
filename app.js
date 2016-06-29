603273951aa2ab4c1f44e81ebd30c22560abb1f1
Ben Pinali <ben@benpinali.com>
2016-06-28 19:08:08 -0500

    Adding make editable directive, editable.html template, CSS rules to highlight edit status

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 var app = angular.module('editableItem', []);
+
+app.directive('makeEditable', function() {
+		return {
+			restrict: 'A',
+			transclude: true,
+			templateUrl: 'editable.html',
+			scope: true,
+		}
+	});
